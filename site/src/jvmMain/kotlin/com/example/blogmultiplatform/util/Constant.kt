f68bf6a1bd41eeb1600b60130f2152a5af8ebe10
Mahbub Alam <56908198+Mahbub58@users.noreply.github.com>
2025-05-01 18:38:42 +0600

    docker created jdk11

--- site/src/jvmMain/kotlin/com/example/blogmultiplatform/util/Constant.kt
+++ site/src/jvmMain/kotlin/com/example/blogmultiplatform/util/Constant.kt
@@ -3,6 +3,7 @@ package com.example.blogmultiplatform.util
 object Constant {
 
      const val DATABASE_NAME = "my_blog"
+
      const val ADMIN_TABLE="admin"
      const val POST_TABLE="post"
      const val SUBSCRIBER="subscriber"
